06d2dffb3af84360c1bf17bb883167a1ae70d46c
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-12-01 20:52:08 +0200

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tema/AutoTema.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tema/AutoTema.java
@@ -54,20 +54,15 @@ public class AutoTema extends LinearOpMode {
     private void move(float leftY, float leftX, float rightX) {
         double max;
 
-        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-        double axial   = -leftY;  // Note: pushing stick forward gives negative value
-        double lateral =  leftX;
-        double yaw     =  rightX;
+        double axial   = leftY;
+        double lateral = leftX;
+        double yaw     = rightX;
 
-        // Combine the joystick requests for each axis-motion to determine each wheel's power.
-        // Set up a variable for each drive wheel to save the power level for telemetry.
         double leftFrontPower  = axial + lateral + yaw;
         double rightFrontPower = axial - lateral - yaw;
         double leftBackPower   = axial - lateral + yaw;
         double rightBackPower  = axial + lateral - yaw;
 
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
         max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
         max = Math.max(max, Math.abs(leftBackPower));
         max = Math.max(max, Math.abs(rightBackPower));
@@ -79,13 +74,11 @@ public class AutoTema extends LinearOpMode {
             rightBackPower  /= max;
         }
 
-        // Send calculated power to wheels
         leftFrontDrive.setPower(leftFrontPower);
         rightFrontDrive.setPower(rightFrontPower);
         leftBackDrive.setPower(leftBackPower);
         rightBackDrive.setPower(rightBackPower);
 
-        // Show the elapsed game time and wheel power.
         telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
         telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
         telemetry.update();
